7f44422031c61b69e5501508b169bd222c4cf373
kuba <jakub.uzialo@gmail.com>
2017-08-20 18:24:40 +0200

    finished task

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ssyys.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      response.courses.completed.forEach(function (entry) {
+        var completedCourse = $('<div class="course"></div>');
+        completedCourse.append($('<h3>' + entry.title + '</h3>'));
+        completedCourse.append($('<img src="' + entry.badge + '"/>'));
+        completedCourse.append($('<a class="btn & btn-primary" href="' + entry.url + '" target="_blank">See Course</a>'));
+        $('#badges').append(completedCourse);
 
+      })
+    }
+  });
 });
